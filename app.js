66052fca183e3113f071431bbb41bcab448bdf5f
CruzMayra <velazquez.leonora@gmail.com>
2017-10-24 09:33:08 -0500

    Actualización de ejercicio

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var comparar = function(array,array2){
 
-  if(array.length != array2.length){
+  if(array.length !== array2.length){
     return false;
-  } else {
-    for (i = 0; i < array.length; i+=)
-
+  }else{
+    for(i = 0; i < array.length; i+=1){
+      if(array[i] !== array2[i]){
+        return false;
+      }
+    }
   }
-    if
-  }
+  return false
 }
 
 
-var array = [1,2,3,4,5,6,7,8,9,10];
-var array = [1,2,3,4,5,6,7,8,9,10];
+var array = [1,2,3,4,5,6,7,8,9,5];
+var array2 = [1,2,3,4,5,6,7,8,9,10];
 
 comparar(array,array2);
